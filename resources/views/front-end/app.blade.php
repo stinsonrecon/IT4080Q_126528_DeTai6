3625c71cc39e4e38315845b45a1ec8ccd07108af
dungvahieu143 <nguyentrunghieu1432000@gmail.com>
2021-11-14 17:22:13 +0700

    abcd

--- resources/views/front-end/app.blade.php
+++ resources/views/front-end/app.blade.php
@@ -10,7 +10,7 @@
     <title>Document</title>
 </head>
 <body>
-    abcdef@include('front-end.components.header')
+    abcd@include('front-end.components.header')
     @yield('content')
 </body>
 </html>
