c40ecd23a30d00df9d7eea5fa5bbc12208eb3575
thuongx1bg <thuongx1bg@gmail.com>
2021-11-23 00:28:02 +0700

    thuong add vvct

--- resources/views/back-end/components/sidebar.blade.php
+++ resources/views/back-end/components/sidebar.blade.php
@@ -47,6 +47,12 @@
                             <i class="fa fa-wallet pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Phương thức thanh toán</span>
                         </a>
                     </li>
+
+                    <li class="mr-3 flex-1">
+                        <a href="{{route('c.index')}}" class="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-red-500">
+                            <i class="fa fa-wallet pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Về với chúng tôi</span>
+                        </a>
+                    </li>
                     
                 </ul>
             </div>
