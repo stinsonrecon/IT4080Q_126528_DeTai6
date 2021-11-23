fde7a21e20fbe03960d4d215af8c7376f891eeb8
hieuBK <nguyentrunghieu1432000@gmail.com>
2021-11-23 21:24:11 +0700

    fix finish productController

--- resources/views/back-end/admin/product/edit.blade.php
+++ resources/views/back-end/admin/product/edit.blade.php
@@ -191,7 +191,7 @@
 
     <div class="flex items-center justify-between">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-       Thêm mới
+       Chỉnh sửa
       </button>
       
     </div>
