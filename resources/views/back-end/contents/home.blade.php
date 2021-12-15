4440676f71e692c99c184a045a07200d8f5bd7f9
stinsonrecon <minhtienlomo@gmail.com>
2021-12-15 20:22:32 +0700

    fix home detail

--- resources/views/back-end/contents/home.blade.php
+++ resources/views/back-end/contents/home.blade.php
@@ -99,7 +99,7 @@
                         "data": {
                             "labels": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
                             "datasets": [{
-                                "label": "Đơn hàng",
+                                "label": "Đơn PAKN",
                                 "data": _xdata,
                                 "fill": false,
                                 "backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)", "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)", "rgb(105, 205, 356, 0.2)", "rgb(52, 256, 108, 0.2)", "rgb(101, 105, 322, 0.2)", "rgb(213, 105, 32, 0.2)", "rgb(187, 113, 205, 0.2)"],
